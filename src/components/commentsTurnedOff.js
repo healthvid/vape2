cf6fb7270e333a5ec438469704c6dade240c0849
Tingting-Jiang <jtting@126.com>
2023-01-09 16:15:31 -0800

    Update styles

--- src/components/commentsTurnedOff.js
+++ src/components/commentsTurnedOff.js
@@ -2,7 +2,9 @@ import React from "react";
 const CommentsTurnedOff = ()=>{
     return (
         <div className="center" id='center'>
-        <div className='fs-3 border border-3 border-secondary d-flex justify-content-center comments-border'>Comments are turned off.</div>
+            <div className="border border-3 border-secondary comments-border">
+                <div className="text-center fs-3 pe-2 ps-2">Comments are turned off. </div>
+            </div>
         </div>
     )
     
